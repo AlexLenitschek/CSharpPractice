7aae21fd3e4f79e3c9cb77d03452b7f430787183
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-05-19 16:31:36 +0200

    Adding more exercises of the method Module of the Microsoft cert.

--- CsharpProjects/TestProjectFirstMethods/Program.cs
+++ CsharpProjects/TestProjectFirstMethods/Program.cs
@@ -486,31 +486,110 @@ davaldes@hayworth.com
 
 #region Exercise: Reverse and Return a String.
 
-string input = "there are snakes at the zoo";
+// string input = "there are snakes at the zoo";
 
-Console.WriteLine(input);
-Console.WriteLine(ReverseSentence(input));
-string ReverseWord(string word)
+// Console.WriteLine(input);
+// Console.WriteLine(ReverseSentence(input));
+// string ReverseWord(string word)
+// {
+//     string result = "";
+//     for (int i = word.Length - 1; i >= 0; i--)
+//     {
+//         result += word[i];
+//     }
+//     return result;
+// }
+
+// string ReverseSentence(string input) 
+// {
+//     string result = "";
+//     string[] words = input.Split(" ");
+
+//     foreach(string word in words) 
+//     {
+//         result += ReverseWord(word) + " ";
+//     }
+
+//     return result.Trim();
+// }
+
+#endregion
+
+#region Exercise: Return booleans from methods.
+
+// string[] words = {"racecar" ,"talented", "deified", "tent", "tenet"};
+
+// Console.WriteLine("Is it a palindrome?");
+// foreach (string word in words) 
+// {
+//     Console.WriteLine($"{word}: {IsPalindrome(word)}");
+// }
+
+// bool IsPalindrome(string word) 
+// {
+//     int start = 0;
+//     int end = word.Length - 1;
+
+//     while (start < end) 
+//     {
+//         if (word[start] != word[end]) 
+//         {
+//             return false;
+//         }
+//         start++;
+//         end--;
+//     }
+
+//     return true;
+// }
+
+#endregion
+
+#region Return Arrays from methods.
+
+int target = 55;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
+
+if (result.Length == 0) 
 {
-    string result = "";
-    for (int i = word.Length - 1; i >= 0; i--)
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++) 
     {
-        result += word[i];
+        if (result[i,0] == -1) 
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
     }
-    return result;
 }
 
-string ReverseSentence(string input) 
+int[,] TwoCoins(int[] coins, int target) 
 {
-    string result = "";
-    string[] words = input.Split(" ");
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    int count = 0;
 
-    foreach(string word in words) 
+    for (int curr = 0; curr < coins.Length; curr++) 
     {
-        result += ReverseWord(word) + " ";
+        for (int next = curr + 1; next < coins.Length; next++) 
+        {
+            if (coins[curr] + coins[next] == target) 
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+            if (count == result.GetLength(0)) 
+            {
+                return result;
+            }
+        }
     }
-
-    return result.Trim();
+    return (count == 0) ? new int[0,0] : result;
 }
 
 #endregion
